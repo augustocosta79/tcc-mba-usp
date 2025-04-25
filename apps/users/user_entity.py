fab8e90394c8b7cc73df97e264f18ee173751165
augustocosta79 <amcneto@hotmail.com>
2025-04-25 17:13:23 -0300

    :recycle: refactor: encapsulate User attributes with properties for better access control

--- apps/users/user_entity.py
+++ apps/users/user_entity.py
@@ -5,19 +5,39 @@ from apps.shared.value_objects.name import Name
 from apps.shared.value_objects.email import Email
 
 class User:
-    id: UUID
-    name: Name
-    email: Email
-    username: Optional[str]
-    created_at: datetime
-    updated_at: datetime
-    is_active: bool
-
     def __init__(self, name: Name, email: Email, username: Optional[str]):
-        self.id = uuid4()
-        self.name = name
-        self.email = email
-        self.username = username
-        self.is_active = True
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self._id = uuid4()
+        self._name = name
+        self._email = email
+        self._username = username
+        self._is_active = True
+        self._created_at = datetime.now()
+        self._updated_at = datetime.now()
+
+    @property
+    def id(self) -> UUID:
+        return self._id
+    
+    @property
+    def name(self) -> Name:
+        return self._name
+    
+    @property
+    def email(self) -> Email:
+        return self._email
+    
+    @property
+    def username(self) -> str:
+        return self._username
+    
+    @property
+    def is_active(self) -> bool:
+        return self._is_active
+    
+    @property
+    def created_at(self) -> datetime:
+        return self._created_at
+    
+    @property
+    def updated_at(self) -> datetime:
+        return self._updated_at
