c2686ca7964a78072d4110837587937bb862ebfb
augustocosta79 <amcneto@hotmail.com>
2025-04-25 13:31:50 -0300

    refactor: rename test method for clarity and update name parameter to use Name value object

--- tests/unit/users/test_service.py
+++ tests/unit/users/test_service.py
@@ -1,21 +1,23 @@
 from unittest.mock import MagicMock
 from apps.users.service import UserService
 from apps.users.user_entity import User
+from apps.shared.value_objects.name import Name
+import pytest
 
 class TestUserService:
-    def test_create_user_with_valid_data(self):
+    def test_should_create_user_with_valid_data(self):
 
         mock_repository = MagicMock()
         service = UserService(repository=mock_repository)
 
         email="amcneto@hotmail.com",
         username="amcneto",
-        name="Augusto"
+        name=Name("Augusto")
         
         user = service.create_user(
+            name=name,
             email=email,
-            username=username,
-            name=name
+            username=username
         )
 
         assert isinstance(user, User)
