88c4a297d50e658efd7e89392743ee4658c9032a
augustocosta79 <amcneto@hotmail.com>
2025-04-25 13:32:37 -0300

    :bug: fix: update create_user method to use Name value object for name parameter

--- apps/users/service.py
+++ apps/users/service.py
@@ -1,10 +1,12 @@
+from typing import Optional
 from apps.users.user_entity import User
+from apps.shared.value_objects.name import Name
 
 class UserService:
     def __init__(self, repository):
         self.repository = repository
 
-    def create_user(self, name: str, email: str, username: str) -> User:
+    def create_user(self, name: Name, email: str, username: Optional[str] = None) -> User:
         user = User(name=name, email=email, username=username)
         self.repository.save(user)
         return user
