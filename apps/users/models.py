2d0cec74ee989f037e708cb1226df8ccfa519384
augustocosta79 <amcneto@hotmail.com>
2025-04-27 22:03:17 -0300

    :sparkles: feat: create initial migration for UserModel with UUID primary key

---
+++ apps/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from uuid import uuid4
+
+class UserModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
+    username = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "users"
